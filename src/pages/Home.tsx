4e804d531bfb58be2287f089d60795eb71551438
Samuel Monção <samuel.santos@redspark.io>
2021-08-13 16:00:43 -0300

    solution challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks(oldState => [...oldState, {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle,
+    }]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(oldState => oldState.map(task => {
+      if(task.id !== id) {
+        return task;
+      }
+
+      return {
+        ...task,
+        done: !task.done,
+      }
+    }));
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
